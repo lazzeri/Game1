feb71f78756efeaf61cfff6e622a794fe91c92c2
Luca <lucalazzeri97@gmail.com>
2018-04-09 20:02:24 +0200

    Movement Integrated

--- Game.1/Assets/Scripts/PlayerMovement.cs
+++ Game.1/Assets/Scripts/PlayerMovement.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour{
 
 	public float moveSpeed;
 	private Rigidbody myRigidbody;
@@ -9,21 +9,45 @@ public class PlayerMovement : MonoBehaviour {
 	private Vector3 moveInput;
 	private Vector3 moveVelocity;
 
+	private int Vertical;
+	private int Horizontal;
+
 	// Use this for initialization
 	void Start () {
+		Vertical = 0;
+		Horizontal = 0;
 		myRigidbody = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		moveInput = new Vector3 (Input.GetAxisRaw("Horizontal"),0f, Input.GetAxisRaw ("Vertical"));
+		moveInput = new Vector3 (Vertical,0f, Horizontal);
 		moveVelocity = moveInput * moveSpeed;
+		print(Vertical);
+	
+	
+	if(Input.GetKey("a")){
+		Vertical = -1;
+	}else if(Input.GetKey("d")){
+		Vertical = 1;
+	} else{
+		Vertical = 0;
+	}
 
+	if(Input.GetKey("s")){
+		Horizontal = -1;
+	}else if(Input.GetKey("w")){
+		Horizontal = 1;
+	} else{
+		Horizontal = 0;
+	}
 
 	}
 
 	void FixedUpdate(){
 		myRigidbody.velocity = moveVelocity;
+		
+		
 	}
 
 
