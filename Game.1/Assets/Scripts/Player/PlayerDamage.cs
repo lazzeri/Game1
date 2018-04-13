81e24e59617f58fd4c23618f0d78bee1b7ef652e
lazzeri <lucalazzeri97@gmail.com>
2018-04-13 15:28:31 +0200

    Updated MeshAgent

--- Game.1/Assets/Scripts/Player/PlayerDamage.cs
+++ Game.1/Assets/Scripts/Player/PlayerDamage.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerStatus : MonoBehaviour {
+public class PlayerDamage : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
