941388cb167e38a4edfc8f694a348ec8aa62b70c
lazzeri <lucalazzeri97@gmail.com>
2018-04-16 19:45:12 +0200

    No clue

---
+++ Game.1/Assets/Scripts/Ambient/SpawnGizmos.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnGizmos : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawCube(transform.position,new Vector3(0.1f,0.1f,0.1f));
+	}
+}
---
+++ Game.1/Assets/SpawnEnemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour {
+
+	public GameObject Enemy;
+	
+	// Use this for initialization
+	void Start () {
+
+	GameObject[] Enemies = new GameObject[4]; 	
+	
+		for(int i = 0; i < 4; i++)
+	{
+		Enemies[i] = ((GameObject)Instantiate(Enemy));
+	}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	public IEnumerator SpawnEnemies()
+	{
+		
+		yield return new WaitForSeconds(0.15f);
+	}
+}
