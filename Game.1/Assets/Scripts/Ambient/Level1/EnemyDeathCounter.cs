142eb0834b7955d35018415d70508a90641c25ee
lazzeri <lucalazzeri97@gmail.com>
2018-04-22 12:51:27 +0200

    PLS wurk

---
+++ Game.1/Assets/Scripts/Ambient/Level1/EnemyDeathCounter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDeathCounter : MonoBehaviour {
+
+	public static int counter = 3;
+	public Animator ACDoor;
+	public GameObject Door;
+
+	// Use this for initialization
+	void Start () {
+		Door = GameObject.Find("RoomCollider and NavMesh/Door");
+		counter = 3;
+		ACDoor = Door.GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void FixedUpdate()
+	{
+		if(counter == 4)
+		{
+			ACDoor.Play("Open");
+		}
+	}
+
+	
+}
---
+++ Game.1/Assets/ToDoList.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToDoList : MonoBehaviour {
+
+	/*
+	
+	Lucas ToDoList:
+	Start into game
+	Enemy Damage
+	PlayerDeath
+	
+	
+	
+	
+	
+	
+	
+	Rott TodoList:
+	Finish Decorating FirstMap: Gas, Schraufen and Light on Door
+	Create UI
+	
+	
+	
+	
+	
+	
+	
+	 */
+}
